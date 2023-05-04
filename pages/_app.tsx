0871dd15a681780d1099f05bdd17b851c37024b9
Hoang Minh Thanh <hmthanhgm@gmail.com>
2023-05-04 23:33:54 +0700

    add Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import '../styles/index.css'
+import {Analytics} from '@vercel/analytics/react';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics/>
+        </>);
 }
